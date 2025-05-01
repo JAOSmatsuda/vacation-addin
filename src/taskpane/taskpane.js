ae7ac08277cc490a7a0c34aee979437cdf1a7cc2
JAOSmatsuda <matsuda@jaos.co.jp>
2025-05-01 16:16:53 +0900

    Add files via upload

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -1,4 +1,4 @@
-console.log("taskpane.js ロード: 2025/05/01 16:01");
+console.log("taskpane.js ロード: 2025/05/01 16:16");
 
 Office.onReady((info) => {
   if (info.host === Office.HostType.Outlook) {
@@ -56,11 +56,11 @@ function setAMVacation() {
   }
 
   const start = new Date();
-  start.setHours(9, 0, 0, 0);
+  start.setUTCHours(0, 0, 0, 0);  // JST 9:00
   console.log("設定する開始時刻: ", start.toISOString());
 
   const end = new Date();
-  end.setHours(13, 0, 0, 0);
+  end.setUTCHours(4, 0, 0, 0);    // JST 13:00
   console.log("設定する終了時刻: ", end.toISOString());
 
   console.log("AM半休設定開始");
