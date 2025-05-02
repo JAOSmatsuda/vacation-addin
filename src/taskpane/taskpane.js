14236eb45efe5cfebf93a1e1aeecd189b36f4122
JAOSmatsuda <matsuda@jaos.co.jp>
2025-05-02 16:50:45 +0900

    Add files via upload

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -1,4 +1,4 @@
-const scriptVersion = "2025/05/02 16:42";
+const scriptVersion = "2025/05/02 16:50";
 
 function updateVersionDisplay() {
   const versionElement = document.getElementById("version");
